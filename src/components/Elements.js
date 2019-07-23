aec44704b8193fbed743d8962598d0b88201d3a4
jorge-francisco-1985 <jorge.francisco@solfine.mx>
2019-07-23 17:44:38 -0500

    Eliminando bootstrap 3

---
+++ src/components/Elements.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Elements extends Component {
+    render() {
+        return (
+            <nav className="header">
+                hola
+            </nav>
+        );
+    }
+}
+export default Elements;
