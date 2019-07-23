4a531667f40415af200ea0a19d2182cc99d374cb
jorge-francisco-1985 <jorge.francisco@solfine.mx>
2019-07-23 17:02:55 -0500

    Agregado archivo header.js

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+class HeaderNav extends Component{
+    render(){
+        return (
+            <nav className="header">
+
+            </nav>
+        );
+    }
+}
+export default HeaderNav;
