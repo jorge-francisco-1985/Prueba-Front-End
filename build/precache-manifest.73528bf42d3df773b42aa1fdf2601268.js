2b1f1e9ac1115258134fa43f389fb3fd5adc45fc
jorge-francisco-1985 <jorge.francisco@solfine.mx>
2019-07-29 15:17:37 -0500

    Revisando la configuración

---
+++ build/precache-manifest.73528bf42d3df773b42aa1fdf2601268.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b169ac700fffda7ef6b4f7fd387805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "952cac28eb0665d81d5d",
+    "url": "/static/css/main.9d37340f.chunk.css"
+  },
+  {
+    "revision": "fa9722a7c8b06809c430",
+    "url": "/static/js/2.61cffa32.chunk.js"
+  },
+  {
+    "revision": "952cac28eb0665d81d5d",
+    "url": "/static/js/main.837766bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
